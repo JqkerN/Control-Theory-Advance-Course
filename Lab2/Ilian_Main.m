d9faa6dd6157f19bc7cdfcf44b730ea7df3cbef6
JqkerN <ilian.corneliussen@gmail.com>
2020-04-04 13:39:34 +0200

    Code done for lab2

--- Lab2/Ilian_Main.m
+++ Lab2/Ilian_Main.m
@@ -113,4 +113,59 @@ phi = pi/3; % Intended phase margin
 wc_mp = 0.1;   % rad/s crossover frequency  (minimum phase case!)
 wc = 0.02;     % rad/s crossover frequency  (NON-minumum phase case!)
 %---------------------------
+%       FUNCTIONS:
+T =@(G, w) tan(phi - pi/2 - angle(evalfr(G, 1i*w))) / w;
+L =@(G, w) G*(1 + 1/(s*T(G,w)));
+K =@(G, w) 1/abs(evalfr(L(G,w), 1i*w));
+f =@(G, w) K(G, w)*(1 + 1/(s*T(G, w)));
+%---------------------------
+G_mp = G;
+f1_mp = f(G_mp(1,1), wc_mp);
+f2_mp = f(G_mp(2,2), wc_mp);
 
+f1_NON = f(G_NON(1,1), wc);
+f2_NON = f(G_NON(2,2), wc);
+
+F_1_mp = [f1_mp, 0;0, f2_mp];
+F_2_mp = [0, f1_mp;f2_mp, 0];
+
+F_1_NON = [f1_NON, 0;0, f2_NON];
+F_2_NON = [0, f1_NON;f2_NON, 0];
+
+L1_mp = G_mp*F_1_mp;
+L2_mp = G_mp*F_2_mp;
+L1 = G_NON*F_1_NON;
+L2 = G_NON*F_2_NON;
+
+disp('plot figure 3211: bode(L1_mp) minphase')
+figure(3211)
+bode(L1_mp); grid on; title('3.2.1(1) Bode(L1_{mp}) minphase');
+disp('plot figure 3212: bode(L2_mp) minphase')
+figure(3212)
+bode(L2_mp); grid on; title('3.2.1(2) Bode(L2_{mp}) minphase');
+
+disp('plot figure 3213: bode(L1)')
+figure(3213)
+bode(L1); grid on; title('3.2.1(3) Bode(L1)');
+disp('plot figure 3214: bode(L2)')
+figure(3214)
+bode(L2); grid on; title('3.2.1(4) Bode(L2)');
+
+
+%% 3.2.2
+disp(' ')
+disp('<strong>---------------------------------------------------</strong>')
+disp('<strong>Task 3.2.2</strong>')
+
+S1 = (eye(2) + L1)^(-1);
+S2 = (eye(2) + L2)^(-1);
+T1 = S1*L1;
+T2 = S2*L2;
+
+S1_mp = (eye(2) + L1_mp)^(-1);
+S2_mp = (eye(2) + L2_mp)^(-1);
+T1_mp = S1_mp*L1_mp;
+T2_mp = S2_mp*L2_mp;
+
+G = G_NON;
+F = F_1_NON;
